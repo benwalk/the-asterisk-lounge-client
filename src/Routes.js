8b199c613031e80771ff51659b657b2960204669
Ben Walker <benwalker@benwalker.local>
2018-06-20 20:25:37 -0700

    added settings page and route

--- src/Routes.js
+++ src/Routes.js
@@ -7,12 +7,18 @@ import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 import Signup from "./containers/Signup";
+import Settings from "./containers/Settings";
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
+    /*Unauthenticated Routes*/
     <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
     <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+
+    /*Authenticated Routes*/
+    <AuthenticatedRoute path="/settings" exact component={Settings} props={childProps} />
+    
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
